0897de9f3057e24674f11e442789de88a07f2668
dheerajrox <cs17b028@iittp.ac.in>
2020-06-14 23:02:21 +0530

    Comparator added

--- src/main/file_map_engine/ast_engine.py
+++ src/main/file_map_engine/ast_engine.py
@@ -1,5 +1,9 @@
 # For testing
-# test_path = 'test\python\examples\\custom_object.py'
+import os
+# from ast_helper import wrapper_get
+
+# test_path = 'D:\Code\AC2\AC2\src\main\\test\Python\\game_of_life\game_o_life.py'
+# # # test_path = os.path.abspath(test_path)
 # code = open(test_path, 'r').read()
 
 import ast
@@ -10,38 +14,42 @@ def classname(cls):
 
 def jsonify_ast(node, level=0):
     fields = {}
-    for k in node._fields:
-        fields[k] = '...'
-        v = getattr(node, k)
-        if isinstance(v, ast.AST):
-            if v._fields:
-                fields[k] = jsonify_ast(v)
+    try:
+        for k in node._fields:
+            fields[k] = '...'
+            v = getattr(node, k)
+            if isinstance(v, ast.AST):
+                if v._fields:
+                    fields[k] = jsonify_ast(v)
+                else:
+                    fields[k] = classname(v)
+
+            elif isinstance(v, list):
+                fields[k] = []
+                for e in v:
+                    fields[k].append(jsonify_ast(e))
+
+            elif isinstance(v, str):
+                fields[k] = v
+
+            elif isinstance(v, int) or isinstance(v, float):
+                fields[k] = v
+
+            elif v is None:
+                fields[k] = None
+
             else:
-                fields[k] = classname(v)
+                fields[k] = 'unrecognized'
 
-        elif isinstance(v, list):
-            fields[k] = []
-            for e in v:
-                fields[k].append(jsonify_ast(e))
-
-        elif isinstance(v, str):
-            fields[k] = v
-
-        elif isinstance(v, int) or isinstance(v, float):
-            fields[k] = v
-
-        elif v is None:
-            fields[k] = None
-
-        else:
-            fields[k] = 'unrecognized'
-
-    ret = { classname(node): fields }
-    return ret
+        ret = { classname(node): fields }
+        return ret
+    except:
+        ret = { classname(node): fields }
 
 def make_ast(code):
     tree = ast.parse(code)
     return jsonify_ast(tree)
 
-# print(make_ast(code))
-
+# new_tree = make_ast(code)
+# final_obj = wrapper_get(new_tree, 'test')
+# print(final_obj)
--- src/main/main.py
+++ src/main/main.py
@@ -5,7 +5,7 @@ from os import path
 from pprint import pprint
 import json
 import ast
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 import sys
 from file_map_engine.engine import get_calls
 from file_map_engine.call_dir import dir_walk
@@ -13,62 +13,140 @@ from file_map_engine.ast_engine import make_ast
 from file_map_engine.ast_helper import wrapper_get
 import pickle
 
-
+cur_dir = os.getcwd()
 # from test import make_ast
 
 app = Flask(__name__)
 
 @app.route('/')
+def new_page():
+    return render_template('home.html')
+
+@app.route('/download_data', methods=['POST'])
+def download():
+    github = request.form['github']
+    print(github)
+
+    cmd = "git clone "+github
+    cmd2 = 'git tag --sort=committerdate > ../tags.txt'
+
+
+    test_dir = cur_dir+'/test'
+    os.chdir(test_dir)
+    os.system(cmd)
+
+    cur = os.getcwd()
+    repo_dir = cur + '\\' + github.split('/')[-1].split('.')[0]
+    os.chdir(repo_dir)
+    os.system(cmd2)
+    os.chdir(test_dir)
+
+    tags = open('tags.txt').readlines()
+
+    os.chdir(repo_dir)
+    
+    ulti_dict = {}
+    ulti_tree = {}
+    
+    num = 0
+
+    for i in tags:
+        num += 1
+        print("\n\n\n New Tag: ", i, "\n\n")
+
+        
+        cmd3 = 'git checkout ' + i
+        os.system(cmd3)
+        
+
+        os.chdir(cur_dir)
+
+
+        new_dir = os.path.join(os.getcwd(), 'test')
+        tree = dir_walk(new_dir)
+
+
+        #https://github.com/poise/python.git
+        returned_tree = get_dictionary(tree)
+
+        ulti_tree[str(num)] = tree
+        ulti_dict[str(num)] = returned_tree
+        
+        os.chdir(repo_dir)
+    
+        
+    
+    os.chdir(cur_dir)
+    pickle.dump( ulti_dict, open( "ulti_dict2.dat", "wb" ))
+    pickle.dump( ulti_tree, open( "ulti_tree2.dat", "wb" ))
+    
+    return redirect('/run')
+
+@app.route('/run')
 def hello_world():
+    
     script_dir = os.getcwd()
+
     # test_path = 'test/file.py'
 
     # test_path = 'test'
+
     new_dir = os.path.join(os.getcwd(), 'test')
     tree = dir_walk(new_dir)
-
+    # new_calls_tree = get_dictionary(tree)
+    
     # print(tree)
 
 
     # This if for engine of parsing.
 
     ########################################################################################
+    new_calls_tree = pickle.load( open( "test2.dat", "rb" ) )
+    ulti_dict = pickle.load( open( "ulti_dict2.dat", "rb" ) )
+    ulti_tree = pickle.load( open( "ulti_tree2.dat", "rb" ) )
 
-    # new_calls_tree = {}
+    tags = open('test/tags.txt').readlines()
 
-    # for k in tree.keys():
-    #     print(k, "\n-------------")
-    #     for i in tree[k]:
-    #         if i['type'] == 'file':
-    #             if i['value'].split('.')[-1] == 'py':
-    #                 print(i['value'])
-    #                 code = open(i['value'], 'r', encoding='utf-8').read()
-    #                 new_tree = make_ast(code)
-    #                 final_obj = wrapper_get(new_tree, i['value'])
-    #                 print(final_obj)
-    #                 print("successful")
-    #                 if i['value'] not in new_calls_tree.keys():
-    #                     new_calls_tree[i['value']] = dict(final_obj)
+    # print(ulti_dict['51'])
+    # print(ulti_dict['6'])
+    # print(ulti_dict['7'])
+    # print(ulti_dict['8'])
     
-    # pickle.dump( new_calls_tree, open( "test.dat", "wb" ))
-    
-    
-    new_calls_tree = pickle.load( open( "test.dat", "rb" ) )
-    print(len(new_calls_tree.keys()))
 
-                    # temp_tree ,temp_mod = get_calls(i['value'])
-                    # print(temp_mod)
+
+    # temp_tree ,temp_mod = get_calls(i['value'])
+    # print(temp_mod)
     
     ########################################################################################
             
 
 
-    return render_template('index.html', tree = tree, calls = new_calls_tree)
+    return render_template('index.html', tags = tags, ulti_tree = {'body': ulti_tree} ,ulti_dict = {'body': ulti_dict}, tree = tree, calls = new_calls_tree)
     # return "hello_world"
 
+def get_dictionary(tree):
+
+    new_calls_tree = {}
+
+    for k in tree.keys():
+        print(k, "\n-------------")
+        for i in tree[k]:
+            if i['type'] == 'file':
+                if i['value'].split('.')[-1] == 'py':
+                    # print(i['value'])
+                    code = open(i['value'], 'r', encoding='utf-8').read()
+                    new_tree = make_ast(code)
+                    final_obj = wrapper_get(new_tree, i['value'])
+                    # print(final_obj)
+                    print("successful")
+                    if i['value'] not in new_calls_tree.keys():
+                        new_calls_tree[i['value']] = dict(final_obj)
+    
+    # pickle.dump( new_calls_tree, open( "test2.dat", "wb" ))
+
+    return new_calls_tree
+
 if __name__ == '__main__':
-
-
 
     app.run(debug = True)
 
